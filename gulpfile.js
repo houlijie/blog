f63670139607cbc7726ab03a6775c9a2941000a9
houlijie <1256485941@qq.com>
2017-04-18 14:44:43 +0800

    [新增]使用gulp将最新的jquery、bootstrap、clean-blog应用到项目中

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,26 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+
+/**
+ * copy clean-blog less files
+ *
+ */
+gulp.task("copyfiles", function(){
+    gulp.src("bower_components/jquery/dist/jquery.js")
+        .pipe(gulp.dest("resources/assets/js/"));
+
+    gulp.src("bower_components/bootstrap/less/**")
+        .pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+    gulp.src("bower_components/bootstrap/dist/js/bootstrap.js")
+        .pipe(gulp.dest("resources/assets/js/"));
+
+    gulp.src("bower_components/bootstrap/dist/fonts/**")
+        .pipe(gulp.dest("public/assets/fonts"));
+
+    gulp.src("bower_components/clean-blog/less/**")
+        .pipe(gulp.dest("resources/assets/less/clean-blog"));
+});
 
 /*
  |--------------------------------------------------------------------------
